c8a33ef9f2eb2dfc2f7adc0e8f96dc35073b136c
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-09-10 13:31:20 -0400

    added open video

--- read.py
+++ read.py
@@ -1,7 +1,19 @@
 import cv2 as cv
 
-img = cv.imread('Photos/cat.jpg')
+# img = cv.imread('Photos/cat.jpg')
+# cv.imshow('Cat', img) # reading photo
 
-cv.imshow('Cat', img)
+# cv.waitKey(0)
 
-cv.waitKey(0)
+capture = cv.VideoCapture(0)
+
+while True:
+	isTrue, frame = capture.read()
+	cv.imshow("Video", frame)
+
+	if cv.waitKey(20) & 0xFF==ord('d'): #if d is pressed break loop
+		break
+
+capture.release()
+
+cv.destroyAllWindows
